e5589ee382fe21550f0456d0a626311c56a26426
kdharsee <komail.dharsee@gmail.com>
2017-12-02 14:44:19 -0500

    Updated Tabbing and Added EMG Recording call

--- NewData.php
+++ NewData.php
@@ -1,12 +1,16 @@
 <html>
-<body>
-<p>
-Model1: <?php echo $_GET["model1"];
-      echo("<script type='text/javascript'> alert('Could you do it for us once more? The thing is, we like your typing!!');</script>");
-        echo("<script type='text/javascript'> window.location.replace('Registration2.html');</script>");
-         
- ?><br>
+  <body>
+	<p>
+	  Model1: <?php echo $_GET["model1"]; ?>
+	  <?php
+		 echo("<script type='text/javascript'> alert('Could you do it for us once more? The thing is, we like your typing!!');</script>");
 
-<p>
-</body>
+		 echo("<script type='text/javascript'> window.location.replace('Registration2.html');</script>");
+
+		 $command = "nohup python myo_access.py record 7 ".$email;
+		 exec( $command, $status );
+
+		 ?><br>
+	<p>
+  </body>
 </html> 
